fd797ae8d1d6a97afcfcc1b813b8e7bddaf90929
CKDXC <ckdxc@qq.com>
2019-05-30 00:35:12 +0800

    online api v1.0

--- src/main/java/com/gospel/data/collection/bilibili/pojo/entity/Online.java
+++ src/main/java/com/gospel/data/collection/bilibili/pojo/entity/Online.java
@@ -8,7 +8,7 @@ import java.util.Date;
 
 /*
  *项目名: data-collection-bilibili
- *文件名: online;//
+ *文件名: online
  *创建者: SCH
  *创建时间:2019/5/29 13:27
  *描述: TODO
@@ -18,26 +18,27 @@ import java.util.Date;
 public class Online {
     @Id
     private long id;
-    private int webOnline;//网页在线人数
-    private int playOnline;//手机在线人数
-    private int allCount;//投稿数
-    private int animaCount;//动画区投稿数
-    private int dramaCount;//番剧区投稿数
-    private int guochuangCount;//国创区投稿数
-    private int musicCount;//音乐区投稿数
-    private int danceCount;//舞蹈区投稿数
-    private int ganmeCount;//游戏区投稿数
-    private int scienceCount;//科技区投稿数
-    private int digitalCount;//数码区投稿数
-    private int lifeCount;//生活区投稿数
-    private int guichuCount;//鬼畜区投稿数
-    private int fashionCount;//时尚区投稿数
-    private int adCount;//广告区投稿数
-    private int varietyCount;//娱乐区投稿数
-    private int movieCount;//影视区投稿数
-    private int projectionCount;//放映厅区投稿数
-    private Date created;//获取时间
-    private int year;//获取年份
-    private int month;//获取月份
-    private int day;//获取号数
+    private int webOnline;          //网页在线人数
+    private int playOnline;         //手机在线人数
+    private int allCount;           //投稿数
+    private int animaCount;         //动画区投稿数(1)
+    private int musicCount;         //音乐区投稿数(3)
+    private int ganmeCount;         //游戏区投稿数(4)
+    private int varietyCount;       //娱乐区投稿数(5)
+    private int dramaCount;         //番剧区投稿数(13)
+    private int part23Count;        //未知分区23投稿数(23)
+    private int scienceCount;       //科技区投稿数(36)
+    private int guichuCount;        //鬼畜区投稿数(119)
+    private int danceCount;         //舞蹈区投稿数(129)
+    private int fashionCount;       //时尚区投稿数(155)
+    private int lifeCount;          //生活区投稿数(160)
+    private int adCount;            //广告区投稿数(165)
+    private int guochuangCount;     //国创区投稿数(167)
+    private int projectionCount;    //放映厅区投稿数(177)
+    private int movieCount;         //影视区投稿数(181)
+    private int digitalCount;       //数码区投稿数(188)
+    private Date created;           //获取时间
+    private int year;               //获取年份
+    private int month;              //获取月份
+    private int day;                //获取号数
 }
--- src/main/java/com/gospel/data/collection/bilibili/service/OnlineThead.java
+++ src/main/java/com/gospel/data/collection/bilibili/service/OnlineThead.java
@@ -7,7 +7,6 @@ import com.gospel.data.collection.bilibili.repository.OnlineRepository;
 import com.gospel.data.collection.bilibili.util.RedisUtil;
 import org.springframework.context.ApplicationContext;
 
-import javax.xml.crypto.Data;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -21,10 +20,10 @@ import java.util.Date;
  *文件名: OnlineThead
  *创建者: SCH
  *创建时间:2019/5/29 9:37
- *描述: TODO
+ *描述: 获取 bilibili 在线人数,各分区投稿数
  */
 public class OnlineThead extends Thread {
-    private String path;
+    private String path = "https://api.bilibili.com/x/web-interface/online?callback=jqueryCallback_bili_18984301554370875&jsonp=jsonp&_=1559098307094";
     private ApplicationContext applicationContext;
     private RedisUtil redisUtil;
     private int no;
@@ -32,11 +31,13 @@ public class OnlineThead extends Thread {
     private int ranS;
     private int waitTime;
 
+    public OnlineThead(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
 
     public OnlineThead(ApplicationContext applicationContext, String path) {
         this.applicationContext = applicationContext;
         this.path = path;
-        this.redisUtil = (RedisUtil) applicationContext.getBean(RedisUtil.class);
     }
 
     @Override
@@ -94,27 +95,27 @@ public class OnlineThead extends Thread {
         online.setPlayOnline((int) dataJson.get("play_online"));
         online.setAllCount((int) dataJson.get("all_count"));
         JSONObject regionJson = dataJson.getJSONObject("region_count");
-        System.out.println("regionJson\t" + regionJson);
-        online.setAnimaCount((int) regionJson.get("1"));       //动画区投稿数(1)
-        online.setDramaCount((int) regionJson.get("13"));      //番剧区投稿数(13)
-        online.setGuochuangCount((int) regionJson.get("13"));  //国创区投稿数(167)
-        online.setMusicCount((int) regionJson.get("13"));      //音乐区投稿数
-        online.setDanceCount((int) regionJson.get("129"));     //舞蹈区投稿数(129)
-        online.setGanmeCount((int) regionJson.get("13"));      //游戏区投稿数
-        online.setScienceCount((int) regionJson.get("13"));    //科技区投稿数
-        online.setDigitalCount((int) regionJson.get("188"));   //数码区投稿数(188)
-        online.setLifeCount((int) regionJson.get("13"));       //生活区投稿数
-        online.setGuichuCount((int) regionJson.get("119"));    //鬼畜区投稿数(119)
-        online.setFashionCount((int) regionJson.get("155"));   //时尚区投稿数(155)
-        online.setAdCount((int) regionJson.get("13"));         //广告区投稿数(165)
-        online.setVarietyCount((int) regionJson.get("13"));    //娱乐区投稿数
-        online.setMovieCount((int) regionJson.get("13"));      //影视区投稿数
-        online.setProjectionCount((int) regionJson.get("13")); //放映厅区投稿数
+        online.setAnimaCount((int) regionJson.get("1"));        //动画区投稿数(1)
+        online.setMusicCount((int) regionJson.get("3"));        //音乐区投稿数(3)
+        online.setGanmeCount((int) regionJson.get("4"));        //游戏区投稿数(4)
+        online.setVarietyCount((int) regionJson.get("5"));      //娱乐区投稿数(5)
+        online.setDramaCount((int) regionJson.get("13"));       //番剧区投稿数(13)
+        online.setPart23Count((int) regionJson.get("23"));       //未知分区23投稿数(23)
+        online.setScienceCount((int) regionJson.get("36"));     //科技区投稿数(36)
+        online.setGuichuCount((int) regionJson.get("119"));     //鬼畜区投稿数(119)
+        online.setDanceCount((int) regionJson.get("129"));      //舞蹈区投稿数(129)
+        online.setFashionCount((int) regionJson.get("155"));    //时尚区投稿数(155)
+        online.setLifeCount((int) regionJson.get("160"));       //生活区投稿数(160)
+        online.setAdCount((int) regionJson.get("165"));         //广告区投稿数(165)
+        online.setGuochuangCount((int) regionJson.get("167"));  //国创区投稿数(167)
+        online.setProjectionCount((int) regionJson.get("177")); //放映厅区投稿数(177)
+        online.setMovieCount((int) regionJson.get("181"));      //影视区投稿数(181)
+        online.setDigitalCount((int) regionJson.get("188"));    //数码区投稿数(188)
         Date now = new Date();
         online.setCreated(now);
-        online.setYear(now.getYear());
-        online.setMonth(now.getMonth()+1);
-        online.setDay(now.getDate()+1);
+        online.setYear(now.getYear() + 1900);
+        online.setMonth(now.getMonth() + 1);
+        online.setDay(now.getDate());
         OnlineRepository dao = applicationContext.getBean(OnlineRepository.class);
         dao.save(online);
         dao.flush();
